eac45954030839b80b5b737bf010ff77f0e7b642
chentao <ct38324@gmail.com>
2017-05-02 14:02:58 +0800

    project done

--- TeachingApp/app/src/main/java/com/chentao/teachingapp/ui/fragments/AuthFragment.java
+++
@@ -1,8 +0,0 @@
-package com.chentao.teachingapp.ui.fragments;
-
-/**
- * Created by chentao on 17-4-21.
- */
-
-public class AuthFragment {
-}
--- TeachingApp/app/src/main/java/com/chentao/teachingapp/ui/fragments/EditStdFragment.java
+++
@@ -1,8 +0,0 @@
-package com.chentao.teachingapp.ui.fragments;
-
-/**
- * Created by chentao on 17-4-20.
- */
-
-public class EditStdFragment {
-}
